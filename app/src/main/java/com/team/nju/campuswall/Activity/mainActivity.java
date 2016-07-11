f17f47dc8b2b9e1506e6e88382b4e8e6691347b0
zrui14 <zrui14@software.nju.edu.cn>
2016-07-11 18:55:46 +0800

    简单的登录布局和数据判断

--- app/src/main/java/com/team/nju/campuswall/Activity/mainActivity.java
+++ app/src/main/java/com/team/nju/campuswall/Activity/mainActivity.java
@@ -1,13 +1,17 @@
 package com.team.nju.campuswall.Activity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class mainActivity extends AppCompatActivity {
+public class mainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(mainActivity.this,LoginActivity.class);
+        startActivity(intent);
     }
 }
