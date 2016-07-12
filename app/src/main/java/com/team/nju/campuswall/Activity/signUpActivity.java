5f64b60f5faa11734a817eff883e94adc275275b
zrui14 <zrui14@software.nju.edu.cn>
2016-07-12 17:57:22 +0800

    做图..还要改...

---
+++ app/src/main/java/com/team/nju/campuswall/Activity/signUpActivity.java
@@ -0,0 +1,16 @@
+package com.team.nju.campuswall.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.team.nju.campuswall.R;
+
+public class signUpActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+}
