32a79c7e85cf5afb128a2898d85545c5e1ca6684
zrui14 <zrui14@software.nju.edu.cn>
2016-07-14 20:06:55 +0800

    个人中心

---
+++ app/src/main/java/com/team/nju/campuswall/Activity/profileActivity.java
@@ -0,0 +1,27 @@
+package com.team.nju.campuswall.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import com.team.nju.campuswall.myView.photoChange;
+
+public class profileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        TableLayout tableLayout =(TableLayout)findViewById(R.id.profileLayout);
+        final photoChange pc = new photoChange(profileActivity.this);
+        tableLayout.addView(pc);
+        ImageView photo = (ImageView)findViewById(R.id.photo);
+        TextView username=(TextView)findViewById(R.id.profile_username);
+        TextView school=(TextView)findViewById(R.id.profile_school);
+        TextView product=(TextView)findViewById(R.id.profile_product);
+        TextView join=(TextView)findViewById(R.id.profile_join);
+        TextView star=(TextView)findViewById(R.id.profile_star);
+    }
+}
