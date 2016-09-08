3ddb2149f8253753c530b229597497600fa49eb4
tq14 <tq14@software.nju.edu.cn>
2016-09-08 15:45:23 +0800

    Development: 更新

---
+++ app/src/main/java/com/team/nju/campuswall/Activity/CommentActivity.java
@@ -0,0 +1,15 @@
+package com.team.nju.campuswall.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.team.nju.campuswall.R;
+
+public class CommentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+    }
+}
