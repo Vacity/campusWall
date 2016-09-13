92bb6ade57779d0069c838b0d8e6f1bace24e47e
tq14 <tq14@software.nju.edu.cn>
2016-09-13 21:43:30 +0800

    发布界面第二版：加上标题和选择类别

---
+++ app/src/main/java/com/team/nju/campuswall/Activity/publishActivity.java
@@ -0,0 +1,26 @@
+package com.team.nju.campuswall.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.team.nju.campuswall.R;
+
+public class publishActivity extends AppCompatActivity {
+
+    EditText title = (EditText) findViewById(R.id.publishTitle);
+    Button emotion = (Button) findViewById(R.id.emotionTitleBtn);
+    Button event = (Button) findViewById(R.id.eventTitleBtn);
+    Button thing = (Button) findViewById(R.id.thingTitleBtn);
+    ImageView add = (ImageView) findViewById(R.id.addIcon);
+    Button publish = (Button) findViewById(R.id.publishBtn);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publish);
+
+    }
+}
