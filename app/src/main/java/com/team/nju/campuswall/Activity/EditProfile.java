10c6e27799039912754822163b04f853e2a7a1cc
zt14 <zt14@software.nju.edu.cn>
2016-10-07 18:07:27 +0800

    编辑信息标题栏

--- app/src/main/java/com/team/nju/campuswall/Activity/EditProfile.java
+++ app/src/main/java/com/team/nju/campuswall/Activity/EditProfile.java
@@ -6,9 +6,14 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.team.nju.campuswall.Network.NetworkCallbackInterface;
@@ -26,10 +31,10 @@ import java.util.Map;
 
 public class EditProfile extends AppCompatActivity implements NetworkCallbackInterface.NetRequestIterface{
     private String[] genderArr = new String[] {"男","女"};
-    private EditText genderText;
-    private EditText nickname;
-    private EditText password;
-    private EditText signature;
+    private TextView genderText;
+    private TextView nickname;
+    private TextView password;
+    private TextView signature;
     private Button save;
 
     private String phone;
@@ -47,6 +52,7 @@ public class EditProfile extends AppCompatActivity implements NetworkCallbackInt
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
         setContentView(R.layout.activity_edit_profile);
 
         Bundle data = getIntent().getExtras();
@@ -57,7 +63,7 @@ public class EditProfile extends AppCompatActivity implements NetworkCallbackInt
         oldsex=data.getString("sex");
 
         requestFragment=new  netRequest(this,this);
-        genderText = (EditText) findViewById(R.id.text_gender);
+        genderText = (TextView) findViewById(R.id.tv_gender);
         genderText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -65,9 +71,9 @@ public class EditProfile extends AppCompatActivity implements NetworkCallbackInt
             }
         });
         save=(Button)findViewById(R.id.save_profile);
-        nickname =(EditText)findViewById(R.id.text_nickName);
-        password =(EditText)findViewById(R.id.text_password);
-        signature=(EditText)findViewById(R.id.text_signature);
+        nickname =(TextView)findViewById(R.id.tv_nickname);
+        password =(TextView)findViewById(R.id.tv_password);
+        signature=(TextView)findViewById(R.id.tv_signature);
         nickname.setText(oldnickname);
         password.setText("");
         signature.setText(oldsignature);
@@ -99,16 +105,16 @@ public class EditProfile extends AppCompatActivity implements NetworkCallbackInt
     性别选择框
      */
     private void showGenderDialog(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setSingleChoiceItems(genderArr , 0 , new DialogInterface.OnClickListener(){  //0表示默认的位置，即“男”
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this).
+                setSingleChoiceItems(genderArr , 0 , new DialogInterface.OnClickListener(){  //0表示默认的位置，即“男”
                     @Override
                     public void onClick(DialogInterface dialog, int which) { //which表示选中的位置
                         genderText.setText(genderArr[which]);
                         dialog.dismiss(); //随便点击一个item，dialog消失，不用点击确认
                     }
-                }
-        );
-        builder.show();
+                });
+
+        alertDialog.show();
     }
 
     @Override
